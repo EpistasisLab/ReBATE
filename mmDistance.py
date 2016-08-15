6c7542caf993bc93a874d89debf18edb51ff3ca3
Pete Schmitt <peter.r.schmitt@icloud.com>
2016-08-15 15:17:20 -0400

    Split row functions for mixed or missing data

--- mmDistance.py
+++ mmDistance.py
@@ -13,13 +13,17 @@ def getDistances(xc, xd, var, cdiffs):
     
     for index in range(var['datalen']):
 
-        row = getrow(xd, xc, cdiffs, index, missing)
+        if(missing > 0):
+            row = getrow_missing(xd, xc, cdiffs, index, missing)
+        else:
+            row = getrow_mixed(xd, xc, cdiffs, index, missing)
+
         row = list(row)
         distArray.append(row)
         
     return distArray
 ###############################################################################
-def getrow(xd, xc, cdiffs, index, missing):
+def getrow_missing(xd, xc, cdiffs, index, missing):
 
     row = empty(0,dtype=np.double)
     dinst1 = xd[index]
@@ -29,33 +33,46 @@ def getrow(xd, xc, cdiffs, index, missing):
         dinst2 = xd[j]
         cinst2 = xc[j]
 
-        if(missing > 0):
-            # continuous
-            an = where(isnan(cinst1))[0]  # find idx of missing data
-            bn = where(isnan(cinst2))[0]
-            idx = unique(append(an,bn))   # create unique list
-            c1 = delete(cinst1,idx)       # remove elements by idx
-            c2 = delete(cinst2,idx)
-            cdf = delete(cdiffs,idx)
+        # continuous
+        an = where(isnan(cinst1))[0]  # find idx of missing data
+        bn = where(isnan(cinst2))[0]
+        idx = unique(append(an,bn))   # create unique list
+        c1 = delete(cinst1,idx)       # remove elements by idx
+        c2 = delete(cinst2,idx)
+        cdf = delete(cdiffs,idx)
 
-            # discrete
-            an = where(isnan(dinst1))[0]
-            bn = where(isnan(dinst2))[0]
-            idx = unique(append(an,bn))
-            d1 = delete(dinst1,idx)
-            d2 = delete(dinst2,idx)
-        else:
-            d1 = dinst1
-            d2 = dinst2
-            c1 = cinst1
-            c2 = cinst2
-            cdf = cdiffs
+        # discrete
+        an = where(isnan(dinst1))[0]
+        bn = where(isnan(dinst2))[0]
+        idx = unique(append(an,bn))
+        d1 = delete(dinst1,idx)
+        d2 = delete(dinst2,idx)
             
         # discrete first
         dist += len(d1[d1 != d2])
 
         # now continuous
         dist += np.sum(np.absolute(np.subtract(c1,c2)) / cdf)
+
+        row = append(row,dist)
+
+    return row
+##############################################################################
+def getrow_mixed(xd, xc, cdiffs, index, missing):
+
+    row = empty(0,dtype=np.double)
+    d1 = xd[index]
+    c1 = xc[index]
+    for j in range(index):
+        dist = 0
+        d2 = xd[j]
+        c2 = xc[j]
+
+        # discrete first
+        dist += len(d1[d1 != d2])
+
+        # now continuous
+        dist += np.sum(np.absolute(np.subtract(c1,c2)) / cdiffs)
 
         row = append(row,dist)
 
