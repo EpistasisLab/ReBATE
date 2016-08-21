ed47be9d54b1e65583c674bb708ed1cdbffcd612
Pete Schmitt <peter.r.schmitt@icloud.com>
2016-08-21 15:15:39 -0400

    16% speed up via smarter logic

--- mmDistance.py
+++ mmDistance.py
@@ -1,7 +1,7 @@
+#Update: Sun Aug 21 15:10:16 EDT 2016
 import sys
 import numpy as np
 from numpy import isnan, where, append, unique, delete, empty
-from IO import printf
 ###############################################################################
 def getDistances(xc, xd, var, cdiffs):
     """ This creates a distance array for mixed types of data with or 
@@ -26,25 +26,25 @@ def getDistances(xc, xd, var, cdiffs):
 def getrow_missing(xd, xc, cdiffs, index, missing):
 
     row = empty(0,dtype=np.double)
-    dinst1 = xd[index]
     cinst1 = xc[index]
+    dinst1 = xd[index]
+    can = where(isnan(cinst1))[0]  # find idx of missing data
+    dan = where(isnan(dinst1))[0]
     for j in range(index):
         dist = 0
         dinst2 = xd[j]
         cinst2 = xc[j]
 
         # continuous
-        an = where(isnan(cinst1))[0]  # find idx of missing data
         bn = where(isnan(cinst2))[0]
-        idx = unique(append(an,bn))   # create unique list
+        idx = unique(append(can,bn))   # create unique list
         c1 = delete(cinst1,idx)       # remove elements by idx
         c2 = delete(cinst2,idx)
         cdf = delete(cdiffs,idx)
 
         # discrete
-        an = where(isnan(dinst1))[0]
         bn = where(isnan(dinst2))[0]
-        idx = unique(append(an,bn))
+        idx = unique(append(dan,bn))
         d1 = delete(dinst1,idx)
         d2 = delete(dinst2,idx)
             
