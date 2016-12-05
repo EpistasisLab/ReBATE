cf4933d86a685aa21e30585d2eebb99e02d60392
Pete Schmitt <peter.r.schmitt@icloud.com>
2016-12-05 15:26:05 -0500

    add copyright

--- mmDistance.py
+++ mmDistance.py
@@ -1,4 +1,25 @@
 # Wed Aug 24 14:51:56 EDT 2016
+"""
+Copyright (c) 2016 Peter R. Schmitt and Ryan J. Urbanowicz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
 import sys
 import numpy as np
 from numpy import isnan, where, append, unique, delete, empty
@@ -67,6 +88,7 @@ def getrow_missing(xc, xd, cdiffs, index, cindices, dindices):
 
     return row
 ##############################################################################
+# if data is clean this will not be used
 def getrow_mixed(xc, xd, cdiffs, index):
 
     row = empty(0,dtype=np.double)
