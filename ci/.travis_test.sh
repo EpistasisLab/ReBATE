f3e7811f9b0f673bbc19e53c89883b2591909564
ryanurbs <ryanurb@upenn.edu>
2018-04-30 18:03:21 -0400

    removed -s

--- ci/.travis_test.sh
+++ ci/.travis_test.sh
@@ -16,7 +16,7 @@ python -c "import sklearn; print('sklearn %s' % sklearn.__version__)"
 python -c "import pandas; print('pandas %s' % pandas.__version__)"
 
 if [[ "$COVERAGE" == "true" ]]; then
-    nosetests -s -v --with-coverage
+    nosetests -v --with-coverage
 else
-    nosetests -s -v
+    nosetests -v
 fi
